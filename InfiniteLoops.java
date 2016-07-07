522d8064d4cfe86f68573fb9c6a4041a94fdb594
ms748 <storost@web.de>
2016-07-07 11:44:48 +0200

    new

---
+++ InfiniteLoops.java
@@ -0,0 +1,30 @@
+package de;
+
+public class InfiniteLoops {
+	
+	public static void main(String[] args) {
+		
+		int i = 0;
+		
+		while(true){
+			System.out.println("A_" + i++);
+			if(i==5) break;
+		}
+
+		i=0;
+		for(;;){
+			System.out.println("B_" + i++);
+			if(i==5) break;
+		}
+		
+		i=0;
+		do{
+          System.out.println("C_" + i++);
+		  if(i==5) break;
+		} while(true);
+		
+		System.out.println("(7-2)*2=" + ((7-2)*2) );
+		System.out.println("7-2*2=" +   (7-2*2) );
+	}
+	
+}
