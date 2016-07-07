8cf6dfb223fb610c7b6668b79f1268dedc53beec
ms748 <axnx@users.noreply.github.com>
2016-07-07 16:54:36 +0200

    Update InfiniteLoops.java

--- InfiniteLoops.java
+++ InfiniteLoops.java
@@ -5,7 +5,6 @@ public class InfiniteLoops {
 	public static void main(String[] args) {
 		
 		int i = 0;
-		
 		while(true){
 			System.out.println("A_" + i++);
 			if(i==5) break;
@@ -19,7 +18,7 @@ public class InfiniteLoops {
 		
 		i=0;
 		do{
-          System.out.println("C_" + i++);
+          	  System.out.println("C_" + i++);
 		  if(i==5) break;
 		} while(true);
 		
